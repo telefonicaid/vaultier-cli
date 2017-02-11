be076c26e332422088da0ec4f2313e3f4d80e309
Óscar García Amor <ogarcia@connectical.com>
2017-02-11 20:51:00 +0100

    Add missing copyright

--- vaultier/views.py
+++ vaultier/views.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
 #
-# Copyright © 2017 Óscar García Amor <ogarcia@connectical.com>
+# Copyright © 2017 Adrián López Tejedor <adrianlzt@gmail.com>
+#                  Óscar García Amor <ogarcia@connectical.com>
 #
 # Distributed under terms of the GNU GPLv3 license.
 
